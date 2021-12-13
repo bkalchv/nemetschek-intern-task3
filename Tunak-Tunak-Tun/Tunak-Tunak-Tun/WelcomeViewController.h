4e15eaa4f45a53ef093c2a585c51531c8e0307ca
bkalchv <bkaltchev98@gmail.com>
2021-12-13 12:28:31 +0200

    adds WelcomeViewController (welcome screen)

---
+++ Tunak-Tunak-Tun/Tunak-Tunak-Tun/WelcomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.h
+//  Tunak-Tunak-Tun
+//
+//  Created by Bogdan Kalchev on 13.12.21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WelcomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tunak-Tunak-Tun/Tunak-Tunak-Tun/WelcomeViewController.m
@@ -0,0 +1,31 @@
+//
+//  WelcomeViewController.m
+//  Tunak-Tunak-Tun
+//
+//  Created by Bogdan Kalchev on 13.12.21.
+//
+
+#import "WelcomeViewController.h"
+
+@interface WelcomeViewController ()
+
+@end
+
+@implementation WelcomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
