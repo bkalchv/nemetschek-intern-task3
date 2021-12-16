0548b48452f106110d30f4e017c352afd2c976c8
bkalchv <bkaltchev98@gmail.com>
2021-12-16 16:16:43 +0200

    adds PreferencesVC

---
+++ Tunak-Tunak-Tun/Tunak-Tunak-Tun/PreferencesViewController.h
@@ -0,0 +1,16 @@
+//
+//  PreferencesViewController.h
+//  Tunak-Tunak-Tun
+//
+//  Created by Bogdan Kalchev on 16.12.21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PreferencesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tunak-Tunak-Tun/Tunak-Tunak-Tun/PreferencesViewController.m
@@ -0,0 +1,31 @@
+//
+//  PreferencesViewController.m
+//  Tunak-Tunak-Tun
+//
+//  Created by Bogdan Kalchev on 16.12.21.
+//
+
+#import "PreferencesViewController.h"
+
+@interface PreferencesViewController ()
+
+@end
+
+@implementation PreferencesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
