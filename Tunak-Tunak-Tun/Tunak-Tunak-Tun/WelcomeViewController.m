c5f7caa9ccf708c7ba309bb3b27f43156954a249
bkalchv <bkaltchev98@gmail.com>
2022-01-26 14:50:30 +0200

    removes everything redundant from WelcomeVC

--- Tunak-Tunak-Tun/Tunak-Tunak-Tun/WelcomeViewController.m
+++ Tunak-Tunak-Tun/Tunak-Tunak-Tun/WelcomeViewController.m
@@ -11,68 +11,14 @@
 #import "FirstPlayerNameInputViewController.h"
 
 @interface WelcomeViewController ()
-@property NSArray<NSString*>* games;
-@property (weak, nonatomic) IBOutlet UIScrollView *gameChoiceScrollView;
-@property (weak, nonatomic) IBOutlet UIPageControl *gameChoicePageControl;
 @property (weak, nonatomic) IBOutlet UIButton *confirmButton;
 @end
 
 @implementation WelcomeViewController
 
-- (void)initGameChoiceScrollView {
-    CGSize contentSize = self.gameChoiceScrollView.frame.size;
-    contentSize.width = self.gameChoiceScrollView.frame.size.width * [self.games count];
-    
-    [self.gameChoiceScrollView setContentSize:contentSize];
-    [self.gameChoiceScrollView setDelegate:self];
-    [self.gameChoiceScrollView setPagingEnabled:YES];
-    [self.gameChoiceScrollView setBounces:NO];
-    [self.gameChoiceScrollView setScrollsToTop:NO];
-    [self.gameChoiceScrollView setScrollEnabled:YES];
-    [self.gameChoiceScrollView setShowsHorizontalScrollIndicator:NO];
-    [self.gameChoiceScrollView setShowsVerticalScrollIndicator:NO];
-    
-    [self.gameChoiceScrollView.layer setBorderWidth:1];
-    [self.gameChoiceScrollView.layer setBorderColor: UIColor.grayColor.CGColor];
-    
-    CGRect scrollViewRect = [self.gameChoiceScrollView bounds];
-    CGFloat scrollViewWidth = scrollViewRect.size.width;
-    CGFloat scrollViewHeight = scrollViewRect.size.height;
-    
-    for (NSUInteger index = 0; index < [self.games count]; ++index) {
-        GamePicturesView* view = [[[NSBundle mainBundle] loadNibNamed:@"GamePicturesView" owner:nil options:nil] objectAtIndex:0];
-        NSString* gameName = [self.games objectAtIndex:index];
-        view.imageView.image = [UIImage imageNamed:gameName];
-        
-        view.frame = CGRectMake(scrollViewWidth * index, 0, scrollViewWidth, scrollViewHeight);
-        [self.gameChoiceScrollView addSubview:view];
-    }
-}
-
--(void)initGameChoicePageControl {
-    [self.gameChoicePageControl setNumberOfPages:[self.games count]];
-    [self.gameChoicePageControl setUserInteractionEnabled:NO];
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self setGames: [[NSArray<NSString*> alloc] initWithObjects: @"TicTacToe", @"TunakTunakTun", nil]];
-    
-    [self initGameChoicePageControl];
-    [self initGameChoiceScrollView];
-}
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-    CGFloat scrollViewWidth = self.gameChoiceScrollView.frame.size.width;
-    self.gameChoicePageControl.currentPage = floor((self.gameChoiceScrollView.contentOffset.x - scrollViewWidth / [self.games count]) / scrollViewWidth) + 1;
-    
-    NSString* chosenGameName = [self.games objectAtIndex: (self.gameChoicePageControl.currentPage)];
-    if ([chosenGameName isEqualToString:@"TicTacToe"]) {
-        [GameConfigurationManager.sharedGameConfigurationManager changeToGame:GameTicTacToe];
-    } else if ([chosenGameName isEqualToString:@"TunakTunakTun"]) {
-        [GameConfigurationManager.sharedGameConfigurationManager changeToGame:GameTunakTunakTun];
-    }
 }
 
 - (void)showPreferencesViewController{
